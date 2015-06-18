cf644c046c9fd362051f91eb37da3a8c48c00e3f
Captain456 <Christopher_Gibbs@student.uml.edu>
2015-06-18 10:30:13 -0400

    Modified MoveArms2.cpp to handle both arms and removed unnecessary lines of code.

--- src/MoveArms2.cpp
+++ src/MoveArms2.cpp
@@ -10,42 +10,49 @@
 #include <sstream>
 #include <cstdlib>
 #include <cmath>
+#include <cstring>
 
 int main(int argc, char** argv)
 {
-	/*namespace gv
-	{
-		class Point{double x; double y; double z;};
-		class Quaternion{double x; double y; double z; double w;};
-		class Pose{Point point; Quaternion quaternion;};
-		class PRY{double pitch; double roll; double yaw;};
-		class PRYPose{Point point; PRY pry;}; 
-	}*/
-
         ros::init(argc, argv, "test_pose");
 
-        if(argc != 7)
+        if(argc != 8)
         {
                 ROS_INFO("Incorrect usage. Try 'pose (position) <x> <y> <z> (orientation) <x> <y> <z> <w>'.\n");
                 exit(1);
         }
 
         ros::NodeHandle n;
+	ros::Publisher pose_pub;
+	ros::ServiceClient client;
 
-        ros::Publisher pose_pub = n.advertise<baxter_core_msgs::JointCommand>("/robot/limb/left/joint_command", 1000);
-        ros::ServiceClient client = n.serviceClient<baxter_core_msgs::SolvePositionIK>("/ExternalTools/left/PositionKinematicsNode/IKService");
+	if(strcmp(argv[1], "left") == 0)
+	{
+        	pose_pub = n.advertise<baxter_core_msgs::JointCommand>("/robot/limb/left/joint_command", 1000);
+        	client = n.serviceClient<baxter_core_msgs::SolvePositionIK>("/ExternalTools/left/PositionKinematicsNode/IKService");
+	}
+	else if(strcmp(argv[1], "right") == 0)
+	{
+		pose_pub = n.advertise<baxter_core_msgs::JointCommand>("/robot/limb/right/joint_command", 1000);
+                client = n.serviceClient<baxter_core_msgs::SolvePositionIK>("/ExternalTools/right/PositionKinematicsNode/IKService");
+	}
+	else
+	{
+		ROS_ERROR("%s You did not enter a valid limb. Try 'left' or 'right'.\n", argv[1]);
+		exit(1);
+	}
         ros::Rate loop_rate(10);
 
         baxter_core_msgs::SolvePositionIK srv;
         double posx, posy, posz, orix, oriy, oriz, oriw, heading, attitude, bank;
         int s0, s1, e0, e1, w0, w1, w2;
        
-        posx = atof(argv[1]);
-        posy = atof(argv[2]);
-        posz = atof(argv[3]);
-        bank = atof(argv[4]);
-        heading = atof(argv[5]);
-        attitude = atof(argv[6]);
+        posx = atof(argv[2]);
+        posy = atof(argv[3]);
+        posz = atof(argv[4]);
+        bank = atof(argv[5]);
+        heading = atof(argv[6]);
+        attitude = atof(argv[7]);
 
     	double mathc1 = cos(heading);
     	double maths1 = sin(heading);
@@ -79,84 +86,13 @@ int main(int argc, char** argv)
                 ROS_INFO("%d", srv.response.isValid[0]);
                 if(srv.response.isValid[0])
                 {
-                        ROS_INFO("Here: Just before the for-loop! %d\n", srv.response.joints[0].name.size());
+			baxter_core_msgs::JointCommand finalPose;
                         for(int i = 0; i < srv.response.joints[0].name.size(); i++)
                         {
-                                ROS_INFO("Here: Inside the for-loop.\n");
-                                ROS_INFO("%s\n", srv.response.joints[0].name[i].c_str());
-
-                                switch(srv.response.joints[0].name[i][5])
-                                {
-                                case 's':
-                                        ROS_INFO("Here: Inside 's'.\n");
-                                        if(srv.response.joints[0].name[i][6] == '0')
-                                        {
-                                                ROS_INFO("Found 's0'.\n");
-                                                s0 = i;
-                                        }
-                                        else
-                                        {
-                                                ROS_INFO("Found 's1'.\n");
-                                                s1 = i;
-                                        }
-                                        break;
-                                case 'e':
-                                        ROS_INFO("Here: Inside 'e'.\n");
-                                        if(srv.response.joints[0].name[i][6] == '0')
-                                        {
-                                                ROS_INFO("Found 'e0'.\n");
-                                                e0 = i;
-                                        }
-                                        else
-                                        {
-                                                ROS_INFO("Found 'e1'.\n");
-                                                e1 = i;
-                                        }
-                                        break;
-                                case 'w':
-                                        ROS_INFO("Here: Inside 'w'.\n");
-                                        if(srv.response.joints[0].name[i][6] == '0')
-                                        {
-                                                ROS_INFO("Found 'w0'.\n");
-                                                w0 = i;
-                                        }
-                                        else if(srv.response.joints[0].name[i][6] == '1')
-                                        {
-                                                ROS_INFO("Found 'w1'.\n");
-                                                w1 = i;
-                                        }
-                                        else
-                                        {
-                                                ROS_INFO("Found 'w2'.\n");
-                                                w2 = i;
-                                        }
-                                        break;
-                                default:
-                                        ROS_INFO("Here: Inside 'default'.\n");
-                                        break;
-                                }
+				finalPose.names.push_back(srv.response.joints[0].name[i]);
+				finalPose.command.push_back(srv.response.joints[0].position[i]);
                         }
-
-                        ROS_INFO("Here: Back outside of the for-loop.\n");
-
-                        baxter_core_msgs::JointCommand finalPose;
                         finalPose.mode = 1;
-                        finalPose.command.push_back(srv.response.joints[0].position[w0]);
-                        finalPose.names.push_back(srv.response.joints[0].name[w0]);
-                        finalPose.command.push_back(srv.response.joints[0].position[w1]);
-                        finalPose.names.push_back(srv.response.joints[0].name[w1]);
-                        finalPose.command.push_back(srv.response.joints[0].position[w2]);
-                        finalPose.names.push_back(srv.response.joints[0].name[w2]);
-                        finalPose.command.push_back(srv.response.joints[0].position[e0]);
-                        finalPose.names.push_back(srv.response.joints[0].name[e0]);
-                        finalPose.command.push_back(srv.response.joints[0].position[e1]);
-                        finalPose.names.push_back(srv.response.joints[0].name[e1]);
-                        finalPose.command.push_back(srv.response.joints[0].position[s0]);
-                        finalPose.names.push_back(srv.response.joints[0].name[s0]);
-                        finalPose.command.push_back(srv.response.joints[0].position[s1]);
-                        finalPose.names.push_back(srv.response.joints[0].name[s1]);
-
-                        ROS_INFO("Here: Past all of the finalPose stuff.\n");
 
                         while(ros::ok)
                         {
