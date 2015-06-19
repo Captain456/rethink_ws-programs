dc4d138a82f05d997e7bbfd2f33efd3499e65fa9
Captain456 <Christopher_Gibbs@student.uml.edu>
2015-06-19 13:06:08 -0400

    Successfully managed to get the robot to move its wrist back and forth in a wave motion.

--- src/WaveMotion.cpp
+++ src/WaveMotion.cpp
@@ -14,6 +14,16 @@
 #include <cmath>
 #include <cstring>
 
+int jointIndex;
+double jointPos;
+
+void callback(sensor_msgs::JointState msg)
+{
+	jointPos = msg.position[jointIndex];
+	ROS_INFO("%f\n", jointPos);
+	return;
+}
+
 int main(int argc, char** argv)
 {
 	ros::init(argc, argv, "test_wave_motion");
@@ -26,21 +36,25 @@ int main(int argc, char** argv)
 
 	ros::NodeHandle n;
 	ros::Publisher armPose_pub;
+	ros::Subscriber state = n.subscribe<sensor_msgs::JointState>("/robot/joint_states", 1000, callback);
 	ros::Rate loop_rate(10);
 	double positions[7];
 	std_msgs::String names[7];
+	int waveState = 0;
 	
 	if(strcmp(argv[1], "left") == 0)
         {
                 armPose_pub = n.advertise<baxter_core_msgs::JointCommand>("/robot/limb/left/joint_command", 1000);
-		
+
 		names[0].data = "left_w1";
+		jointIndex = 7;
 	}
         else if(strcmp(argv[1], "right") == 0)
         {
                 armPose_pub = n.advertise<baxter_core_msgs::JointCommand>("/robot/limb/right/joint_command", 1000);
-
 		names[0].data = "right_w1";
+		jointIndex = 14;
+
 	}
 	else
 	{
@@ -48,9 +62,9 @@ int main(int argc, char** argv)
                 exit(1);
         }
 
-	positions[0] = -1;
+	positions[0] = 1;
 
-	baxter_core_msgs::JointCommand waveHandPose;
+	baxter_core_msgs::JointCommand waveHandPose, waveHandPose2;
 	for(int i = 0; i < 6; i++)
         {
                 waveHandPose.names.push_back(names[i].data);
@@ -58,11 +72,45 @@ int main(int argc, char** argv)
         }
         waveHandPose.mode = 1;
 
+	positions[0] = -1;
+	for(int i = 0; i < 6; i++)
+        {
+                waveHandPose2.names.push_back(names[i].data);
+                waveHandPose2.command.push_back(positions[i]);
+        }
+        waveHandPose2.mode = 1;
+
         while(ros::ok())
         {
-                armPose_pub.publish(waveHandPose);
+		if(waveState == 0)
+		{
+			if(jointPos <= 0.95)
+			{
+				ROS_INFO("Positive!\n");
+				armPose_pub.publish(waveHandPose);
+			}
+			else
+			{
+				ROS_INFO("Changed state to 1.\n");
+				waveState = 1;
+			}
+		}
+		else
+		{
+			if(jointPos >= -0.95)
+			{
+				ROS_INFO("Negative!\n");
+				armPose_pub.publish(waveHandPose2);
+			}
+			else
+			{
+				ROS_INFO("Changed state to 0.\n");
+				waveState = 0;
+			}
+		}
+			
                 ros::spinOnce();
-                loop_rate.sleep();
+		loop_rate.sleep();
         }
 
 
